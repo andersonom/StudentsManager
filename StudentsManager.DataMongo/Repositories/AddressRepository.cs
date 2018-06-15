87998a220879774829db41913fb48cd6501cda91
amartins <amartins@acessocard.com.br>
2018-06-15 11:20:15 -0300

    Mong Data Layer + dotnet core and EF 2.1 update

---
+++ StudentsManager.DataMongo/Repositories/AddressRepository.cs
@@ -0,0 +1,15 @@
+﻿using StudentManager.Domain.Interfaces.Repositories;
+using StudentsManager.DataMongo.Context;
+using StudentsManager.Domain.Models;
+
+
+namespace StudentsManager.DataMongo.Repositories
+{
+    public class AddressRepository : RepositoryBase<Address>, IAddressRepository
+    {
+        public AddressRepository(StudentsManagerContext context) : base(context)
+        {
+       
+        }
+    }
+}
---
+++ StudentsManager.DataMongo/Repositories/RepositoryBase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using StudentManager.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using StudentsManager.DataMongo.Context;
+using StudentsManager.Domain.Bases;
+
+namespace StudentsManager.DataMongo.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected StudentsManagerContext _context;
+
+        public RepositoryBase(StudentsManagerContext context)
+        {
+            _context = context;
+        }
+
+        public IMongoCollection<TEntity> Get()
+        {
+            return _context.database.GetCollection<TEntity>("");
+        }
+
+        public void Add(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaginatedList<TEntity>> GetAllPagedAsync(int? pageSize, int? page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(int id, TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepositoryBase<TEntity>.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
